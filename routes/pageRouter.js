b1085b2094e0c77103b42e06e02af595f20d8664
ozge <karabayozge@gmail.com>
2024-12-10 16:02:37 +0300

    custom auth middleware

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,10 +1,15 @@
-const express=require("express");
-const { getIndexPage, getAboutPage, getRegisterPage, getLoginPage } = require("../controllers/pageControllers");
+const express = require('express');
+const {
+  getIndexPage,
+  getAboutPage,
+  getRegisterPage,
+  getLoginPage,
+} = require('../controllers/pageControllers');
+const redirectMiddleware = require('../middlewares/redirectMiddleware');
+const router = express.Router();
 
-const router=express.Router();
-
-router.route("/").get(getIndexPage);
-router.route("/about").get(getAboutPage);
-router.route("/register").get(getRegisterPage);
-router.route("/login").get(getLoginPage);
-module.exports=router;
+router.route('/').get(getIndexPage);
+router.route('/about').get(getAboutPage);
+router.route('/register').get(redirectMiddleware, getRegisterPage);
+router.route('/login').get(redirectMiddleware, getLoginPage);
+module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,10 @@
 const express=require("express");
 const { createUser, loginUser, getDashboardPage, logoutUser } = require("../controllers/authControllers");
-
-
+const authmiddleware=require("../middlewares/authMiddleware")
 const router=express.Router();
 
 router.route("/signup").post(createUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
-router.route('/dashboard').get(getDashboardPage);
+router.route('/dashboard').get(authmiddleware,getDashboardPage);
 module.exports=router;
