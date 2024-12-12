caa2c48a0668983c0fa1ba7cc67dbdf864af0657
ozge <karabayozge@gmail.com>
2024-12-13 00:21:38 +0300

    update courses

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { createCourse, getAllCourses, getCourse, enrollCourse, releaseCourse, deleteCourse } = require("../controllers/courseControllers");
+const { createCourse, getAllCourses, getCourse, enrollCourse, releaseCourse, deleteCourse, updateCourse } = require("../controllers/courseControllers");
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const router=express.Router();
 
@@ -9,4 +9,5 @@ router.route("/:slug").get(getCourse);
 router.route('/enroll').post(enrollCourse);
 router.route('/release').post(releaseCourse);
 router.route('/:slug').delete(deleteCourse);
+router.route('/:slug').put(updateCourse);
 module.exports=router;
