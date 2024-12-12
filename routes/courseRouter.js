5eebc675f021cdbaf64e8456e5e3eddccc46988c
ozge <karabayozge@gmail.com>
2024-12-12 17:27:16 +0300

    course enroll

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,10 +1,10 @@
 const express=require("express");
-const { createCourse, getAllCourses, getCourse } = require("../controllers/courseControllers");
+const { createCourse, getAllCourses, getCourse, enrollCourse } = require("../controllers/courseControllers");
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const router=express.Router();
 
 router.route('/').post(roleMiddleware(["teacher", "admin"]), createCourse);
 router.route("/").get(getAllCourses);
 router.route("/:slug").get(getCourse);
-
+router.route('/enroll').post(enrollCourse);
 module.exports=router;
