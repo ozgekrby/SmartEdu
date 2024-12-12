ea35155f03f2797d3bf187b878a822a538c25a40
ozge <karabayozge@gmail.com>
2024-12-12 18:38:59 +0300

    contact && mail with ethereal

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,5 +1,6 @@
+const nodemailer = require('nodemailer');
 exports.getIndexPage = (req, res) => {
-  console.log(req.session.userID)
+  console.log(req.session.userID);
   res.status(200).render('index', {
     page_name: 'index',
   });
@@ -23,4 +24,49 @@ exports.getLoginPage = (req, res) => {
   });
 };
 
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+    <h1>Mail Details </h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+  `;
 
+  try {
+
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user, 
+        pass: testAccount.pass, 
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <no-reply@smartedu.com>', 
+      to: 'test@example.com', 
+      subject: 'Smart EDU Contact Form New Message ✔', 
+      html: outputMessage, 
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.status(200).redirect('contact');
+  } catch (error) {
+    console.error('Error while sending email:', error);
+    res.status(500).json({ message: 'Email sending failed', error });
+  }
+};
--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -4,6 +4,8 @@ const {
   getAboutPage,
   getRegisterPage,
   getLoginPage,
+  getContactPage,
+  sendEmail,
 } = require('../controllers/pageControllers');
 const redirectMiddleware = require('../middlewares/redirectMiddleware');
 const router = express.Router();
@@ -12,4 +14,6 @@ router.route('/').get(getIndexPage);
 router.route('/about').get(getAboutPage);
 router.route('/register').get(redirectMiddleware, getRegisterPage);
 router.route('/login').get(redirectMiddleware, getLoginPage);
+router.route('/contact').get(getContactPage);
+router.route('/contact').post(sendEmail);
 module.exports = router;
